bcd4267581c8127b2bfd220a82bf0475bdc5b739
Daniela C <daniela.28chg@gmail.com>
2025-05-18 13:08:46 +0200

    test quitar tildes con acentos

--- tests/FuncionesTest.php
+++ tests/FuncionesTest.php
@@ -5,5 +5,10 @@ use App\Funciones;
 
 final class FuncionesTest extends TestCase
 {
-    
+    public function testQuitarTildesConTextoConAcentos(): void
+    {
+        $texto = "áéíóú ÁÉÍÓÚ";
+        $resultado = Funciones::quitarTildes($texto);
+        $this->assertEquals("aeiou AEIOU", $resultado);
+    }
 }
