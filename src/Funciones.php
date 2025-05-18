d8dae111ffd913dbfac7bd3678499ad83dc9aaf4
Carmenge2 <carmenge2@hotmail.com>
2025-05-18 11:28:58 +0200

    funcion procesar texto

--- src/Funciones.php
+++ src/Funciones.php
@@ -22,11 +22,36 @@ class Funciones
         return $stopwords;
     }
 
-    
+
      public static function quitarTildes(string $texto): string
     {
         $originales = ['á','é','í','ó','ú','Á','É','Í','Ó','Ú'];
         $sinTilde   = ['a','e','i','o','u','A','E','I','O','U'];
         return str_replace($originales, $sinTilde, $texto);
     }
+
+     public static function procesarTexto(string $texto, array $stopwords): array
+    {
+        $texto = mb_convert_encoding($texto, 'UTF-8', 'auto');
+        $texto = mb_strtolower($texto);
+        $texto = self::quitarTildes($texto);
+        $texto = preg_replace('/[^a-zñü\s]/iu', ' ', $texto); // solo letras, sin puntuación ni números
+        $palabrasRaw = explode(' ', $texto);
+
+        $frecuencias = [];
+
+        foreach ($palabrasRaw as $palabra) {
+            $palabra = trim($palabra);
+            if ($palabra !== '' && !isset($stopwords[$palabra])) {
+                if (!isset($frecuencias[$palabra])) {
+                    $frecuencias[$palabra] = 1;
+                } else {
+                    $frecuencias[$palabra]++;
+                }
+            }
+        }
+
+        arsort($frecuencias);
+        return $frecuencias;
+    }  
 }
